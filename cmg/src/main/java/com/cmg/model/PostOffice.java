51df7526f1fa05f18ed953ed6587ab013ba154ab
sree akhil <chandika.sreeharsha@gmail.com>
2020-04-19 10:20:10 +0530

    undo

--- cmg/src/main/java/com/cmg/model/PostOffice.java
+++ cmg/src/main/java/com/cmg/model/PostOffice.java
@@ -1,73 +1,29 @@
 package com.cmg.model;
-
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
-
 //POLICE
-@Entity
-@Table(name="postoffice")
 public class PostOffice {
-	
-	@Id
-	@Column(name="area")
-	private String area;
-	
-	@Column(name="state")
 	private String state;
-	
-	@Column(name="district")
 	private String district;
-	
-	@Column(name="city")
 	private String city;
-
-	@Column(name="zipcode")
-	private String zipCode;
-	
-	public PostOffice() {
-		super();
-		// TODO Auto-generated constructor stub
-	}
-	
-	
-	public PostOffice(String area, String state, String district, String city) {
-		super();
-		this.area = area;
-		this.state = state;
-		this.district = district;
-		this.city = city;
-	}
-
-
-	
-
+	private String area; //pk
 
 	public String getState() {
 		return state;
 	}
-
 	public void setState(String state) {
 		this.state = state;
 	}
-
 	public String getDistrict() {
 		return district;
 	}
-
 	public void setDistrict(String district) {
 		this.district = district;
 	}
-
 	public String getCity() {
 		return city;
 	}
-
 	public void setCity(String city) {
 		this.city = city;
 	}
-
 	public String getZipCode() {
 		return zipCode;
 	}
@@ -80,6 +36,5 @@ public class PostOffice {
 	public void setArea(String area) {
 		this.area = area;
 	}
-
-	
+	private String zipCode;
 }
--- cmg/src/main/java/com/cmg/model/UserForm.java
+++ cmg/src/main/java/com/cmg/model/UserForm.java
@@ -1,127 +1,59 @@
 package com.cmg.model;
-import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 //USER
 @Entity
 @Table(name="userform")
 public class UserForm {
-	
-	@Id
-	@Column(name="name")
 	private String name;
-	
-	@Column(name="contact")
 	private String contact;
-	
-	@Column(name="zipcode")
 	private String zipcode;
-
-	
-	@ManyToOne
-	@JoinColumn(name="area")
-	private PostOffice postOffice;
-	
-	@Column(name="profession")
-
-
+	private String area; //fk
 	private String profession;
-	
-	@Column(name="ministryordepartment")
 	private String ministryOrDept;
-	
-	@Column(name="suggestionorissue")
 	private String suggestionOrIssueText;
-	
-	@Column(name="submissiontype")
 	private String submissionType;
-
-	public UserForm() {
-		super();
-		// TODO Auto-generated constructor stub
-	}
-
-	public UserForm(String name, String contact, String zipcode, PostOffice postOffice, String profession,
-			String ministryOrDept, String suggestionOrIssueText, String submissionType) {
-		super();
-		this.name = name;
-		this.contact = contact;
-		this.zipcode = zipcode;
-		this.postOffice = postOffice;
-		this.profession = profession;
-		this.ministryOrDept = ministryOrDept;
-		this.suggestionOrIssueText = suggestionOrIssueText;
-		this.submissionType = submissionType;
-	}
-
 	public String getName() {
 		return name;
 	}
-
 	public void setName(String name) {
 		this.name = name;
 	}
-
 	public String getContact() {
 		return contact;
 	}
-
 	public void setContact(String contact) {
 		this.contact = contact;
 	}
-
 	public String getZipcode() {
 		return zipcode;
 	}
-
 	public void setZipcode(String zipcode) {
 		this.zipcode = zipcode;
 	}
-
-	public PostOffice getPostOffice() {
-		return postOffice;
-	}
-
-	public void setPostOffice(PostOffice postOffice) {
-		this.postOffice = postOffice;
-	}
-
 	public String getProfession() {
 		return profession;
 	}
-
 	public void setProfession(String profession) {
 		this.profession = profession;
 	}
-
 	public String getMinistryOrDept() {
 		return ministryOrDept;
 	}
-
 	public void setMinistryOrDept(String ministryOrDept) {
 		this.ministryOrDept = ministryOrDept;
 	}
-
 	public String getSuggestionOrIssueText() {
 		return suggestionOrIssueText;
 	}
-
 	public void setSuggestionOrIssueText(String suggestionOrIssueText) {
 		this.suggestionOrIssueText = suggestionOrIssueText;
 	}
-
 	public String getSubmissionType() {
 		return submissionType;
 	}
-
 	public void setSubmissionType(String submissionType) {
 		this.submissionType = submissionType;
-
 	}
-
 	
-	
-	}
+}
